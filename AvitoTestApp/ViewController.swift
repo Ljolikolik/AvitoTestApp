d46df0e61c3ac7a2fdd7df8b7a671780765ca5bb
Olga <olga.shovgeneva@gmail.com>
2023-08-24 16:21:19 +0300

    Initial Commit

---
+++ AvitoTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTestApp
+//
+//  Created by Ольга Шовгенева on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
